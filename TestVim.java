b4fbc6aa7cdb1030d8894e4042729d32452b530b
Nerd2dian0 <815761813@qq.com>
2018-10-27 16:10:06 +0800

    write a file readme.txt

---
+++ TestVim.java
@@ -0,0 +1,14 @@
+/*
+*author:sijunchao
+*time:18.11.10
+*description:testVim and testGit
+*/
+
+package com.sjc
+
+public class TestVim{
+	public static void main(String[] args){
+        System.out.println("hello,vim. i setted your .vimrc!");
+        System.out.println("change again3!");
+	}
+} 
